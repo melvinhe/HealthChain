05fc4bca51b9c4be37521862e172ad1f4a661f02
Melvin He <melvin_he@brown.edu>
2023-06-17 22:21:53 -0400

    other page

---
+++ decentra-app/src/pages/home/OtherPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const OtherPage: FC = () => {
+  return (
+    <div>
+      <h3 style={{ color: "black" }}>
+        Put Your Health Data Back in Your Control: Embrace HealthChain, Securing Your EHR with Blockchain.
+      </h3>
+      {/* Add additional content specific to the OtherPage */}
+    </div>
+  );
+};
+
+export default OtherPage;
