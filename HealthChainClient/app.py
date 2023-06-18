299538cb7361333e6c45fb2175fe567f83950cf9
jaykk128 <jaykk128@yahoo.com>
2023-06-18 01:28:33 -0400

    Add flask framework and encryption functions

--- HealthChainClient/app.py
+++ HealthChainClient/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello Healthchain'
+
+
+@app.route('/encode-patient-data', methods=["POST"])
+def encode_data():  # put application's code here
+    return request.get_json()
 
 
 if __name__ == '__main__':
